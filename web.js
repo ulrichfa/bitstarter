5cdb7c2aeda0fb7c4698114de18e959c978be8da
Juan Lanus <juan.lanus@gmail.com>
2013-07-21 14:49:35 +0000

    index styled - no content

--- web.js
+++ web.js
@@ -8,7 +8,8 @@ app.get('/', function(request, response) {
   response.send( salutation.toString() );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log("http://ec2-54-235-55-224.compute-1.amazonaws.com:8080/");
 });
