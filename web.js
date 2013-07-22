95764002f2bc10620eb4d67b3249b6b37c95aef9
Juan Lanus <juan.lanus@gmail.com>
2013-07-22 03:17:47 +0000

    two missing files

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var salutation = fs.readFileSync('index.html');
-
+app.use('/imag', express.static(__dirname + '/imag'));
+app.use(express.static(__dirname + '/'));
 app.get('/', function(request, response) {
   response.send( salutation.toString() );
 });
@@ -12,4 +13,5 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
   console.log("http://ec2-54-235-55-224.compute-1.amazonaws.com:8080/");
+  console.log("Directory: " + __dirname);
 });
